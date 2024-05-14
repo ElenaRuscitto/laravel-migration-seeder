c6d4ab0f6e4795d8f8655c27f7371e50dabc9de3
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-05-14 16:03:14 +0200

    [add] fakers

---
+++ database/seeders/TrainsTableSeederFakers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+
+class TrainsTableSeederFakers extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 100; $i++) {
+           $new_train = new Train;
+           $new_train->agency = $faker->word(10, true);
+           $new_train->departure_station = $faker->word(2, true);
+           $new_train->arrival_station = $faker->word(2, true);
+           $new_train->departure_time = $faker->time();
+           $new_train->arrival_time = $faker->time();
+           $new_train->train_code = $faker->randomLetter(10);
+           $new_train->number_carriages = $faker->randomDigitNotNull();
+           $new_train->is_time = $faker->boolean();
+           $new_train->deleted = $faker->boolean();
+
+           $new_train->save();
+        }
+    }
+}

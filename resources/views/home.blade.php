1d2fb4e1ee76e547aff1caa4c5df1102005adae0
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-05-14 16:29:20 +0200

    [add] print table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,39 @@
 
 @section('content')
 
-    <h1>Home</h1>
+ <div class="container">
+    <h1>I Treni</h1>
+
+            <table class="table ">
+                <table class="table table-striped">
+                    <thead>
+
+                      <tr>
+                        <th scope="col">Azienda</th>
+                        <th scope="col">Stazione di partenza</th>
+                        <th scope="col">Stazione di arrivo</th>
+                        <th scope="col">Orario di partenza</th>
+                        <th scope="col">Orario di arrivo</th>
+                        <th scope="col">Codice Treno</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+
+                     @foreach ($trains as $train)
+                        <tr>
+                            <th scope="row">{{$train->agency}}</th>
+                            <td>{{$train->departure_station}}</td>
+                            <td>{{$train->arrival_station}}</td>
+                            <td>{{$train->departure_time}}</td>
+                            <td>{{$train->arrival_time}}</td>
+                            <td>{{$train->train_code}}</td>
+                        </tr>
+                      @endforeach
+
+                    </tbody>
+                  </table>
+            </table>
+
+ </div>
 
 @endsection
